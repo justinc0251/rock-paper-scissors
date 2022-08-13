2145ec94ac6d6b1171d100de231a3c61e1cc652b
Justin Chung <jdchung@scu.edu>
2022-08-12 17:11:18 -0700

    Added Random Computer Choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+function getComputerChoice(number)
+{
+    if(number == 0){
+        return "Rock";
+    }
+    else if(number == 1){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
+
+let computerChoice = getComputerChoice(Math.floor(Math.random() * 3));
